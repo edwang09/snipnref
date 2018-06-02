5b5359eafb7f3129a864357036000944255637f5
Nameedwang09 <edwang09@gmail.com>
2018-06-02 00:56:05 -0400

    deploy

--- my-app/src/pages/packages/reactjs/navlist.js
+++ my-app/src/pages/packages/reactjs/navlist.js
@@ -1,6 +1,3 @@
 module.exports = [
-  { name: "Home", link: "/snippets/reactjs/getstart", cat: "getstart" },
-  { name: "Basics", link: "/snippets/reactjs/basic", cat: "basic" },
-  { name: "Redux", link: "/snippets/reactjs/redux", cat: "redux" },
-  { name: "Useful Utilities", link: "/snippets/reactjs/utils", cat: "utils" }
+  { name: "Home", link: "/snippets/reactjs/getstart", cat: "getstart" }
 ];
