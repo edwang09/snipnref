a99d34f67a4054e11bce3c28f237108188415c29
Jianan Wang <edwang09@gmail.com>
2018-07-31 22:24:35 -0400

    faster api

--- routes/fengshui/api.js
+++ routes/fengshui/api.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const request = require("request");
+const fs = require("fs");
 
 //万年历api
 router.post("/calendar", (req, res) => {
-    const jiuweiurl = "https://open-api.9vdata.com/get_perpetual_calendar"
+  const filename = `./routes/fengshui/past/${req.body.date}.json`;
+  if (fs.existsSync(filename)) {
+    var obj = JSON.parse(fs.readFileSync(filename));
+    res.send(obj);
+  } else {
+    const jiuweiurl = "https://open-api.9vdata.com/get_perpetual_calendar";
     if (req.body.date && req.body.secret) {
-        request.post({
-                url: jiuweiurl,
-                form: {
-                    date: req.body.date,
-                    secret: req.body.secret
-                }
-            },
-            function (error, response, body) {
-                res.send(JSON.parse(body));
-            }
-        );
+      request.post(
+        {
+          url: jiuweiurl,
+          form: {
+            date: req.body.date,
+            secret: req.body.secret
+          }
+        },
+        function(error, response, body) {
+          fs.writeFile(filename, body);
+          res.send(JSON.parse(body));
+        }
+      );
     } else {
-        res.send({
-            error: "Parameters of date and secret required"
-        });
+      res.send({
+        error: "Parameters of date and secret required"
+      });
     }
+  }
 });
 
 module.exports = router;
