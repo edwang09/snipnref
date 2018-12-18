5f366cf5cc001481b9117966c6be5fd2ecafef42
Jianan Wang <edwang09@gmail.com>
2018-12-17 22:43:02 -0500

    update

--- my-app/src/pages/demos/bazi/bazi.js
+++ my-app/src/pages/demos/bazi/bazi.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { DatePicker, Card } from "antd";
 import taichi from "./taichi.png";
 import isEmpty from "../../../utils/is-empty";
 import BaziInfo from "./baziInfo"
@@ -9,7 +8,10 @@ class Bazi extends Component {
     super(props);
 
     this.state = {
-      dateString: "",
+      month:1,
+      date:1,
+      year:1991,
+      hour:0,
       tiangan: BaziInfo.tiangan,
       dizhimonthref: BaziInfo.dizhimonthref,
       dizhihourref: BaziInfo.dizhihourref,
@@ -24,39 +26,31 @@ class Bazi extends Component {
     };
   }
 
-  fixMonth = extractdate => {
-    if (isEmpty(extractdate)) {
-      throw new Error("Parameter is not valid!");
-    } else {
-      const { month, day } = extractdate;
+  fixMonth = () => {
+      const { month, date } = this.state;
       var newmonth = month;
-      if (this.state.monthfixer[month - 1] > day) {
+      if (this.state.monthfixer[month - 1] > date) {
         //day before jieqi
       } else {
         newmonth = newmonth + 1;
       }
-      return { ...extractdate, month: newmonth };
-    }
+      return newmonth;
   };
-  getPillars = extractdate => {
-    if (isEmpty(extractdate)) {
-      throw new Error("Parameter is not valid!");
-    } else {
+  getPillars = () => {
       const datediff =
         36000000 +
         Math.round(
-          (new Date(extractdate.year, extractdate.month - 1, extractdate.day) -
+          (new Date(this.state.year, this.state.month - 1, this.state.date) -
             new Date(2018, 4, 31)) /
             (1000 * 60 * 60 * 24)
         );
-      const { year, month, hour } = this.fixMonth(extractdate);
+      const { year, hour } = this.state;
+      const month = this.fixMonth();
       const yearPillar =
         this.state.tiangan[(year - 3) % 10] + this.state.dizhi[(year - 3) % 12];
       const monthPillar =
         this.state.dizhimonthref[(year - 3) % 5][month % 12] +
         this.state.dizhi[month % 12];
-
-      console.log(datediff % 60);
       const dayPillar =
         this.state.tiangan[datediff % 10] + this.state.dizhi[datediff % 12];
       const hournumber = Math.floor(((hour + 1) % 24) / 2 + 1) % 12;
@@ -64,67 +58,85 @@ class Bazi extends Component {
         this.state.dizhihourref[datediff % 5][hournumber] +
         this.state.dizhi[hournumber];
       return { yearPillar, monthPillar, dayPillar, hourPillar };
-    }
   };
-  extractpillars = dateString => {
-    if (typeof dateString === "string" && dateString !== "") {
-      const year = parseInt(dateString.split(" ")[0].split("-")[0], 10);
-      const month = parseInt(dateString.split(" ")[0].split("-")[1], 10);
-      const day = parseInt(dateString.split(" ")[0].split("-")[2], 10);
-      const hour = parseInt(dateString.split(" ")[1].split(":")[0], 10);
-      return { year, month, day, hour };
-    }
-    return {};
-  };
-  onChange = e => (value, dateString) => {
-    this.setState({ dateString });
+  onChange = () => e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
   calculate = () => {
-    console.log("Formatted Selected Time: ", this.state.dateString);
-    const pillars = this.extractpillars(this.state.dateString);
-    this.setState({ pillars: this.getPillars(pillars) });
-  };
-
-  onOk = e => value => {
-    console.log("onOk: ", value);
+    this.setState({ pillars: this.getPillars() });
   };
   render() {
+    const dateSelect = [...Array(31).keys()].map((str, id)=>{
+      return (<option value={id+1}>{id+1}</option>)
+    })
+    const yearSelect = [...Array(200).keys()].map((str, id)=>{
+      return (<option value={id+1900}>{id+1900}</option>)
+    })
+    const hourSelect = [...Array(24).keys()].map((str, id)=>{
+      return (<option value={id}>{id+" : 00"}</option>)
+    })
     return (
-      <div className="container p-3 shadow  main">
-        <h1 className="display-5 text-center">
+      <div className="bazi">
+        <h1>
           Calculate your Four Pillars of Destiny
         </h1>
-        <h2 className="lead p-3">
-          <i className="fas fa-info-circle" /> The Four Pillars of Destiny is a
+        <p>
+          The Four Pillars of Destiny is a
           Chinese astrological concept that a person's destiny or fate can be
           divined by the two sexagenary cycle characters assigned to their birth
           year, month, day, and hour. This type of astrology is also used in
           Japan and Korea.
-        </h2>
-        <h2 className="lead p-3">
-          1. In order to accuratly calculate your Four Pillars of Destiny,
+        </p>
+        <p >
+          In order to accuratly calculate your Four Pillars of Destiny,
           information including your birth year, month, day, and hour needs to
           be provided.
-        </h2>
-        <div className="row justify-content-md-center">
-          <div className="col-md-12 col-lg-6">
-            <h2 className="lead p-3">
-              <i className="fas fa-calendar-alt" /> Enter your Birth Date and
-              Time
-            </h2>
-            <DatePicker
-              className="d-block"
-              dropdownClassName="d-block"
-              showTime
-              format="YYYY-MM-DD HH:mm"
-              placeholder="Please enter your Date of Birth"
-              onChange={this.onChange()}
-              onOk={this.onOk()}
-              renderExtraFooter={() => "Time can be accurate into a minute."}
-            />
+        </p>
+        <hr/>
+        <form className="form" id="baziform">
+          <div className="formgroup">
+            <label htmlFor="month">Month:</label>
+            
+            <select name="month" form="baziform" value={this.state.month}
+              onChange={this.onChange()}>
+              <option value="1">Janurary</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
           </div>
-        </div>
-        <div className="row justify-content-center">
+          <div className="formgroup">
+            <label htmlFor="date">Date:</label>
+            
+            <select name="date" form="baziform" value={this.state.date}
+              onChange={this.onChange()}>
+              {dateSelect}
+            </select>
+          </div>
+          <div className="formgroup">
+            <label htmlFor="year">Year:</label>
+            <select name="year" form="baziform" value={this.state.year}
+              onChange={this.onChange()}>
+              {yearSelect}
+            </select>
+          </div>
+          <div className="formgroup">
+            <label htmlFor="hour">Hour:</label>
+            <select name="hour" form="baziform" value={this.state.hour}
+              onChange={this.onChange()}>
+              {hourSelect}
+            </select>
+          </div>
+        </form>
+        <div className="submit">
           <a href="#result">
             <img
               src={taichi}
@@ -132,41 +144,38 @@ class Bazi extends Component {
               width="200rem"
               height="200rem"
               onClick={this.calculate.bind(this)}
-              className="rounded mx-auto d-block m-5 taichi-icon"
+              className="taichi"
             />
           </a>
         </div>
-        <h2 className="lead p-3">
-          2. Here are the results. Contact Mian Yin for detail explanations.
-        </h2>
-        <div className="row justify-content-center text-center" id="result">
-          <div className="col-md-6 col-lg-3 p-3">
-            <Card title="年柱(Year Pillar)">
-              <p className="pillar-result m-auto">
-                {this.state.pillars.yearPillar}
-              </p>
-            </Card>
+        <hr/>
+        <p>
+          Here are the results. Contact Mian Yin for detail explanations.
+        </p>
+        <div className="result" id="result">
+          <div className="result__pillar">
+            <h3>年柱(Year Pillar)</h3>
+            <p className="result__text">
+              {this.state.pillars.yearPillar}
+            </p>
           </div>
-          <div className="col-md-6 col-lg-3 p-3">
-            <Card title="月柱(Month Pillar)">
-              <p className="pillar-result m-auto">
+          <div className="result__pillar">
+            <h3>月柱(Month Pillar)</h3>
+              <p className="result__text">
                 {this.state.pillars.monthPillar}
               </p>
-            </Card>
           </div>
-          <div className="col-md-6 col-lg-3 p-3">
-            <Card title="日柱(Day Pillar)">
-              <p className="pillar-result m-auto">
+          <div className="result__pillar">
+            <h3>日柱(Day Pillar)</h3>
+              <p className="result__text">
                 {this.state.pillars.dayPillar}
               </p>
-            </Card>
           </div>
-          <div className="col-md-6 col-lg-3 p-3">
-            <Card title="时柱(Hour Pillar)">
-              <p className="pillar-result m-auto">
+          <div className="result__pillar">
+            <h3>时柱(Hour Pillar)</h3>
+              <p className="result__text">
                 {this.state.pillars.hourPillar}
               </p>
-            </Card>
           </div>
         </div>
       </div>
